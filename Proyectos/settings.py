a4c43188912572f76e7c14ccc61472e92eec1816
Luyinq <85760533+Luyinq@users.noreply.github.com>
2022-11-18 19:42:49 -0300

    Update settings.py

--- Proyectos/settings.py
+++ Proyectos/settings.py
@@ -169,6 +169,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static')
+) 
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
